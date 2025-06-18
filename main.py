1a26e3be7f569fd7728d740e3575bb6ab223a4ad
23fi066tami <23fi066@ms.dendai.ac.jp>
2025-06-18 15:40:09 +0900

    Add homepage maker

--- main.py
+++ main.py
@@ -1,24 +1,19 @@
 from fastapi import FastAPI
-import random
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
-
-@app.get("/omikuji")
-def omikuji():
-    omikuji_list = [
-        "大吉",
-        "中吉",
-        "小吉",
-        "吉",
-        "半吉",
-        "末吉",
-        "末小吉",
-        "凶",
-        "小凶",
-        "大凶"
-    ]
-    return {"result": random.choice(omikuji_list)}
+@app.get("/index")
+def index():
+    html_content = """
+    <html>
+        <head>
+            <title>My Web Page</title>
+        </head>
+        <body>
+            <h1>こんにちは！これは私のWebページです！</h1>
+            <p>好きなようにHTMLを書き換えてOK！</p>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
