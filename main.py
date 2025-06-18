ea136d03af83fb02f622dba44f10fad3dd873e57
23fi066tami <23fi066@ms.dendai.ac.jp>
2025-06-18 16:02:30 +0900

    Add main.py

--- main.py
+++ main.py
@@ -18,6 +18,6 @@ def index():
     """
     return HTMLResponse(content=html_content, status_code=200)
 
-   @app.post("/present")
+@app.post("/present")
 async def give_present(present: str = Form(...)):
     return {"response": f"メリークリスマス！{present}ありがとう！お返しはケーキです。"}
